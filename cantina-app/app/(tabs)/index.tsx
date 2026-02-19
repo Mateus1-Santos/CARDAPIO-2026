03909df161ca66b87f7d8688667f6c7170e90f71
DIEGO DE NEGREIROS SOARES <diegodenegreirossoares@gmail.com>
2026-02-19 09:32:40 -0300

    tela inicial

--- cantina-app/app/(tabs)/index.tsx
+++ cantina-app/app/(tabs)/index.tsx
@@ -1,24 +1,33 @@
-import {Text, View, TouchableOpacity, StyleSheet, Image, ScrollView} from 'react-native';
+import {Text, View, TouchableOpacity, StyleSheet, Image, ScrollView,} from 'react-native';
+
  
-export default function HomeScreen() {
+export default function HomeScreen() { 
   return (
-    <View>
-      <Text>Bem Vindo!</Text>
-      <Text>Fazer login</Text>
+    <TouchableOpacity
+    style={styles.botao}
+    onPress={() => alert=("clicou")}
+    >
+
+    <Text>login</Text>
+    </TouchableOpacity>
 
 
-    </View>
-    
+
     
   );
 }
-
+ 
 const styles = StyleSheet.create({
   container:{
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
     padding: 20,
+    
+  }
+
+  botao:{
+    padding : 10
     
   }
 });
