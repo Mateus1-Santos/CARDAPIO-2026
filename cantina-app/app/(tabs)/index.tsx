37ac80434df5f9c7a385e790b3751677b55864c2
welli363 <toncosta08@gmail.com>
2026-02-19 09:09:36 -0300

    Update index.js

--- cantina-app/app/(tabs)/index.tsx
+++ cantina-app/app/(tabs)/index.tsx
@@ -2,38 +2,23 @@ import {Text, View, TouchableOpacity, StyleSheet, Image, ScrollView} from 'react
  
 export default function HomeScreen() {
   return (
-    <>
     <View>
-      
+      <Text>Bem Vindo!</Text>
+      <Text>Fazer login</Text>
+
+
     </View>
     
     
-    
-    
-    
-    
-    </>
-
-
-         
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+  container:{
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+    
+  }
 });
