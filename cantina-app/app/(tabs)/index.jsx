c4e75e3da5808cdb0f347355e5288a23909e2734
DIEGO DE NEGREIROS SOARES <diegodenegreirossoares@gmail.com>
2026-02-19 10:14:47 -0300

    comita la

--- cantina-app/app/(tabs)/index.jsx
+++ cantina-app/app/(tabs)/index.jsx
@@ -3,12 +3,23 @@ import { Text, View, TouchableOpacity, StyleSheet, Image, ScrollView } from 'rea
 export default function HomeScreen() { 
   return (
     <View style={styles.container}>
+      
+      <TouchableOpacity
+        style={styles.botao}
+        onPress={() => alert("quicou")} 
+      >
+        <Text>cadastro</Text>
+      </TouchableOpacity>
+
+      <View style={{ height: 20 }} />
+
       <TouchableOpacity
         style={styles.botao}
         onPress={() => alert("quicou")} 
       >
         <Text>login</Text>
       </TouchableOpacity>
+
     </View>
   );
 }
@@ -16,13 +27,17 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: "center", 
+    alignItems: "center",     
     padding: 20,
+    backgroundColor: '#fff',  
   }, 
 
   botao: { 
-    padding: 10,
+    padding: 15,              
     backgroundColor: '#DDDDDD', 
+    borderRadius: 8,          
+    width: '80%',             
+    alignItems: 'center',
   },
 });
