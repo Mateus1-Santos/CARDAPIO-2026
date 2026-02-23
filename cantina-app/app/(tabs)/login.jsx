178c75d0c1d68c613942b27613d309e63464f63f
DIEGO DE NEGREIROS SOARES <diegodenegreirossoares@gmail.com>
2026-02-23 08:45:54 -0300

    tela de login

---
+++ cantina-app/app/(tabs)/login.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Text, View, TouchableOpacity, StyleSheet, ImageBackground } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+      <view>
+      styles={Styles.text}
+        <text>prencha os campos obrigatorios</text>
+    </view>
+      );
+    }
+ export default function login(){
+
+    return(
+    <View>
+        intu
+    </View>
+    );
+ }
+
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+  },
+});
